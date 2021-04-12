a7d747f66dd7629dd7428dfc8045a64ed495eaa8
Aiman Kayad <Akaya005@fiu.edu>
2021-04-12 14:55:27 -0400

    Create FrameDisplay.java

---
+++ FrameDisplay.java
@@ -0,0 +1,38 @@
+/*
+ *
+ * @author Aiman Kayad PID: 5975108
+ * @author Kevin Hernandez PID: 5634615
+ * 
+ * Section: U01
+ * 
+ * Sources credited that provided me with guidance:
+ * - Professor Hernandez
+ * - https://stackoverflow.com/users/3182664/marco13
+ * - https://stackoverflow.com/users/230513/trashgod
+ */
+
+
+    import javax.swing.*;
+
+public class FrameDisplay extends JFrame
+{
+    int WIDTH = (450);
+    int HEIGHT = (450);
+    
+     ReadInputFromTextFile newRead = new ReadInputFromTextFile();
+    
+   
+     Graph graphy = new Graph(newRead.numberOfVertices);
+    
+    
+    
+    public FrameDisplay()
+    {
+        setTitle("Graph Display");
+        setSize(WIDTH, HEIGHT);
+        
+
+        GraphDisplay panel = new GraphDisplay(newRead.numberOfVertices);
+        add(panel);
+    }
+}
