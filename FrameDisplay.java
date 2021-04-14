2a86e4edb535b5dc87347db565c8a3e8360c483f
Aiman Kayad <Akaya005@fiu.edu>
2021-04-13 23:06:03 -0400

    Update FrameDisplay.java

--- FrameDisplay.java
+++ FrameDisplay.java
@@ -16,8 +16,8 @@
 
 public class FrameDisplay extends JFrame
 {
-    int WIDTH = (450);
-    int HEIGHT = (450);
+    int WIDTH = (900);
+    int HEIGHT = (900);
     
      ReadInputFromTextFile newRead = new ReadInputFromTextFile();
     
